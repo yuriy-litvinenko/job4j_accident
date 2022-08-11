147b40fcbe68ee61869797d57d02cc5dd3423452
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2022-08-11 22:48:29 +0600

    0. Spring Security [#6879]

--- src/main/java/ru/job4j/accident/WebInit.java
+++ src/main/java/ru/job4j/accident/WebInit.java
@@ -5,6 +5,7 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import ru.job4j.accident.config.DataConfig;
+import ru.job4j.accident.config.SecurityConfig;
 import ru.job4j.accident.config.WebConfig;
 
 import javax.servlet.FilterRegistration;
@@ -15,7 +16,7 @@ public class WebInit implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletCxt) {
         AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.register(WebConfig.class, DataConfig.class);
+        ac.register(WebConfig.class, DataConfig.class, SecurityConfig.class);
         ac.refresh();
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
